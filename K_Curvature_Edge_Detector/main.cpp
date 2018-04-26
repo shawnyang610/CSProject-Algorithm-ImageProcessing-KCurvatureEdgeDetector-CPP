a8afec7dec0d48a85dd77d5d826ed1a5f82c1e52
Shawn Yang <shawnyang610@gmail.com>
2018-04-25 22:15:45 -0400

    Initial Commit

---
+++ K_Curvature_Edge_Detector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  K_Curvature_Edge_Detector
+//
+//  Created by Shawn Yang on 4/25/18.
+//  Copyright © 2018 Shawn Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
